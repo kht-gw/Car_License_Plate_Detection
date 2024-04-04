5be0d8661d73064cc0fa8b8e5c98a145f853c4f1
khaing <khainghsuthwe@globalwalkers.co.jp>
2024-04-04 15:51:01 +0630

    modify test code

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -7,9 +7,14 @@ mdate : Friday March 29th 2024
 copyright: 2024 GlobalWalkers.inc. All rights reserved.
 """
 
-import unittest
-from src.license_plate.pipeline import Pipeline
 import os
+import unittest
+
+from src.license_plate.pipeline import Pipeline
+from src.license_plate.utils.weight_downloader import (
+    FILE_ID,
+    download_file_from_google_drive,
+)
 
 
 class test_image(unittest.TestCase):
@@ -17,6 +22,12 @@ class test_image(unittest.TestCase):
         self.pipeline = Pipeline()
 
     def test_image(self):
+        if not os.path.exists("weights"):
+            os.mkdir("weights")
+
+        if not os.path.exists("weights/license_plate_detector.pt"):
+            weight_file_path = os.path.join("weights", "license_plate_detector.pt")
+            download_file_from_google_drive(FILE_ID, weight_file_path)
         text = ""
         current_dir = os.getcwd()
         img_path = os.path.join(current_dir, "assets/images/Cars297.png")
